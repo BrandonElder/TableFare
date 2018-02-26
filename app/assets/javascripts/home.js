4f016e636f056f552fb65c8e46c72b1e1bdf6262
Brandon Elder <brandonelder3@gmail.com>
2018-02-26 02:18:51 +0000

    navbar fixes & font work

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,7 +5,23 @@ function navMenu() {
   });
 }
 
+function navScroll() {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+}
+
+function navAbout() {
+  $('.nav-about').click(function() {
+    $('html,body').animate({
+      scrollTop: $('.about-tf').offset().top},
+      'slow');
+      return false;
+  });
+}
 
 document.addEventListener("turbolinks:load", function() {
   navMenu();
+  navScroll();
 })
