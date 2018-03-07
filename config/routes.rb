d506c8d4a08455a0e14df4b4f04a0f7bb9f39bbb
Brandon Elder <brandonelder3@gmail.com>
2018-03-07 01:52:17 +0000

    added bare bones mailer in contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   
   root 'pages#home'
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/faqs'
-  get 'pages/contact'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'faqs', to: 'pages#faqs'
+  get 'contact-me', to: 'messages#contact', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
 
 end
